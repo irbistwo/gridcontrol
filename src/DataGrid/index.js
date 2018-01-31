dbcca16f4a04af9a5d9a7f4dd0a5f9e695c65762
Fikret Bulatov <frt550@gmail.com>
2018-01-31 09:35:55 +0300

    setData method for updating table context

--- src/DataGrid/index.js
+++ src/DataGrid/index.js
@@ -7,6 +7,31 @@ import './styles.css';
 
 class DataGrid extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      rows: props.data.rows || [],
+      footer: props.data.footer || []
+    }
+  }
+
+  updateData(data) {
+    if (data.rows) {
+      this.setState({ rows: data.rows });
+    }
+
+    if (data.footer) {
+      this.setState({ footer: data.footer });
+    }
+  }
+
+  handleUpdateDataButtonClick() {
+    this.updateData({
+      rows: [...this.state.rows.slice(1), this.state.rows[0]],
+      footer: [...this.state.footer.slice(1), this.state.footer[0]]
+    });
+  }
+
   prepareColumnsData(columns, property) {
     return columns.reduce((result, column) => {
       return column.subColumns ?
@@ -27,14 +52,15 @@ class DataGrid extends Component {
     const columnWidths = this.prepareColumnsData(data.columns, 'width');
     const columnAligns = this.prepareColumnsData(data.columns, 'align');
 
-    return (
+    return [
       <div className="data-grid">
         <DataGridCaption caption={data.caption} />
         <DataGridHeader columns={data.columns} columnLevels={data.columnLevels} />
-        <DataGridBody rows={data.rows} fields={fields} columnWidths={columnWidths} columnAligns={columnAligns} />
-        <DataGridFooter rows={data.footer} columnWidths={columnWidths} columnAligns={columnAligns} />
-      </div>
-    );
+        <DataGridBody rows={this.state.rows} fields={fields} columnWidths={columnWidths} columnAligns={columnAligns} />
+        <DataGridFooter rows={this.state.footer} columnWidths={columnWidths} columnAligns={columnAligns} />
+      </div>,
+      <input type="submit" value="Update data" onClick={this.handleUpdateDataButtonClick.bind(this)}/>
+    ];
   }
 }
 
